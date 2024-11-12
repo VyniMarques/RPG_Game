9eb788cb45e9edd6ca1ca16a158734dd227942fd
Vinícius Marques <vinicius.ximenes@ges.inatel.br>
2024-11-12 11:51:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from pygame.locals import *
 import pygame
+from pygame.locals import *
 
-from utils.combat import reset_battle, hide_mouse, DamageText
-from utils.non_combat import buy_upgrade, message, text_update
+from utils.combat import (
+    reset_battle,
+    hide_mouse,
+    getTarget,
+    playerTurn,
+    enemyTurn,
+    gameOver,
+)
 from utils.draw import (
     draw_bg,
     draw_panel,
@@ -11,10 +17,10 @@ from utils.draw import (
     draw_hero_hud,
     handle_cursor,
 )
-from utils.loop import handle_events
-from utils.loop import basic
+from utils.non_combat import buy_upgrade, message, text_update
 from utils.selectEnemies import selectEnemies
 from utils.selectHero import selectHero
+from utils.loop import handle_events, basic
 from utils.variables import *
 from utils.instances import *
 
@@ -131,7 +137,6 @@ def city():
 
         pygame.display.update()
 
-#from utils.combat import
 
 def combat(place):
     global game_over, current_fighter, action_cooldown
@@ -158,139 +163,61 @@ def combat(place):
         potion = False
         target = None
 
+        pos = pygame.mouse.get_pos()
+
         if game_over == 0:
             if run_button.draw() and current_fighter == 1:
                 reset_battle(hero, enemies)
                 city()
-
         if potion_button.draw():
             potion = True
 
         draw_hero_hud(hero)
-
         for i, enemy in enumerate(enemies):
             draw_update(enemy)
             health_bars[i].draw(enemy.hp)
 
         draw_update(hero)
-
         # Draw damage text
         text_update(damage_text_group, screen)
-
-        pos = pygame.mouse.get_pos()
 
         # Change mouse icon
         hide_mouse(pos, enemies)
 
         # Target enemy
-        for count, enemy in enumerate(enemies):
-            if enemy.hitbox.collidepoint(pos):
-                if clicked == True and enemy.alive == True:
-                    attack = True
-                    target = enemies[count]
-
-        if hero_turn == 1:
-            print("Hero Turn")
-        if enemy_turn == 1:
-            print("Enemy Turn")
+        attack, target = getTarget(enemies, pos)
 
         if game_over == 0:
             # Player action
-            if hero.alive == True:
-                if current_fighter == 1:
-                    action_cooldown += 1
-                    if action_cooldown >= action_wait_time:
-                        hero_turn += 1
-                        # Look for player action
-
-                        # Attack
-                        if attack == True and target != None:
-
-                            hero.attack(target, damage_text_group)
-                            current_fighter += 1
-                            action_cooldown = 0
-                            hero_turn = 0
-
-                        # Potion
-                        if potion == True:
-
-                            if hero.potions > 0:
-                                # Check heal beyond max helth
-                                if hero.max_hp - hero.hp > potion_effect:
-                                    heal_amount = potion_effect
-                                else:
-                                    heal_amount = hero.max_hp - hero.hp
-                                hero.hp += heal_amount
-                                hero.potions -= 1
-                                damage_text = DamageText(
-                                    hero.hitbox.centerx,
-                                    hero.hitbox.y,
-                                    str(heal_amount),
-                                    green,
-                                )
-                                damage_text_group.add(damage_text)
-                                current_fighter += 1
-                                action_cooldown = 0
-                                hero_turn = 0
-            else:
-                game_over = -1
+            hero_turn, game_over, current_fighter, action_cooldown = playerTurn(
+                hero,
+                hero_turn,
+                target,
+                damage_text_group,
+                current_fighter,
+                action_cooldown,
+                attack,
+                potion,
+            )
 
             # Enemy action
             for count, enemy in enumerate(enemies):
-                if current_fighter == 2 + count:
-                    if enemy.alive:
-                        action_cooldown += 1
-                        enemy_turn += 1
-                        if action_cooldown >= action_wait_time:
-                            # Cheack if enemy need to heal
-                            if (enemy.hp / enemy.max_hp) < 0.5 and enemy.potions > 0:
-                                # Check heal beyond max helth
-                                if enemy.max_hp - enemy.hp > potion_effect:
-                                    heal_amount = potion_effect
-                                else:
-                                    heal_amount = enemy.max_hp - enemy.hp
-                                enemy.hp += heal_amount
-                                damage_text = DamageText(
-                                    enemy.hitbox.centerx,
-                                    enemy.hitbox.y,
-                                    str(heal_amount),
-                                    green,
-                                )
-                                damage_text_group.add(damage_text)
-                                enemy.potions -= 1
-                                current_fighter += 1
-                                action_cooldown = 0
-                                enemy_turn = 0
-                            else:
-                                # Attack
-                                enemy.attack(hero, damage_text_group)
-                                current_fighter += 1
-                                action_cooldown = 0
-                                enemy_turn = 0
-                    else:
-                        current_fighter += 1
+                enemy_turn, game_over, current_fighter, action_cooldown = enemyTurn(
+                    enemy,
+                    enemy_turn,
+                    hero,
+                    damage_text_group,
+                    current_fighter,
+                    action_cooldown,
+                    count,
+                )
 
             # If all fighters have had a turn then reset
             if current_fighter > total_fighters:
                 current_fighter = 1
 
-        # Check if all enemies are dead
-        alive_enemies = 0
-        for enemy in enemies:
-            if enemy.alive == True:
-                alive_enemies += 1
-        if alive_enemies == 0:
-            game_over = 1
-
         # Check if game is over
-        if game_over != 0:
-            if game_over == 1:
-                screen.blit(victory_img, (250, 50))
-            if game_over == -1:
-                screen.blit(defeat_img, (290, 50))
-            if restart_button.draw():
-                reset_battle(hero, enemies)
-                city()
+        gameOver(hero, enemies, city, game_over)
 
         clicked = handle_events()
 
