f46fc058eed4310b28eb56e47ceceabdd4273813
Vinícius Marques <vinicius.ximenes@ges.inatel.br>
2025-04-28 16:02:21 -0300

    Update: improving and summarizing functions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 from pygame.locals import *
 
 from utils.combat import (
@@ -19,63 +21,43 @@ from utils.draw import (
     handle_cursor,
     draw_turn_indicator,
     draw_mission_panel,
+    draw_menu_screen,
 )
 from utils.non_combat import buy_upgrade, message, text_update
 from utils.selectEnemies import selectEnemies
-from utils.selectHero import selectHero
+from utils.selectHero import selectHero, handle_hero_selection
 from utils.loop import handle_events, basic
 from utils.variables import *
 from utils.instances import *
 from utils.quest import MissionManager
 
+
 pygame.init()
+
 mission_manager = MissionManager()
 
 
 def menu():
     global hero, hero_in_city
     pygame.mouse.set_visible(True)
-    chosen = False
 
     while True:
+        draw_menu_screen()
 
-        screen.fill(black)
-        draw_bg("map", (800, 550))
-        clock.tick(fps)
-
-        draw_text("RPG Game", title, black, 350, 20)
-        draw_text("Select your hero:", font, black, 325, 65)
-
-        buttons = [
-            knight_button,
-            archer_button,
-            assasin_button,
-            monk_button,
-            priestess_button,
-            mauler_button,
-        ]
-
-        mouse_pos = pygame.mouse.get_pos()
-
-        for i, button in enumerate(buttons):
-            if button.rect.collidepoint(mouse_pos):
-                pygame.draw.rect(screen, (255, 0, 0), button.rect, 3)
-            if button.draw():
-                hero_op = i
-                chosen = True
-                break
-
-        if chosen:
+        hero_op = handle_hero_selection()
+        if hero_op is not None:
             hero, hero_in_city = selectHero(hero_op)
             pygame.time.delay(200)
             city()
+            break
 
-        clicked = handle_events()
+        handle_events()
         pygame.display.update()
 
 
 def map():
     pygame.mouse.set_visible(True)
+    clicked = False
 
     while True:
 
@@ -93,9 +75,10 @@ def map():
                     pygame.event.pump()
                 if place == "city":
                     city()
+                    break
                 else:
                     combat(place)
-                break
+                    break
 
         if potion_button.draw():
             potion = True
@@ -111,8 +94,6 @@ def city():
     pygame.mouse.set_visible(True)
     cursor_hidden = False
     clicked = False
-
-    click_released = True
 
     npcs = [hero_in_city, blacksmith, merchant, beggar, dog]
 
@@ -140,30 +121,37 @@ def city():
 
         mouse_buttons = pygame.mouse.get_pressed()
 
-        if not mouse_buttons[0]:  # Botão esquerdo solto
-            click_released = True
-
-        if not clicked and click_released and quest_button.draw():
+        if quest_button.draw() and len(mission_manager.missions) < 3:
             print("Botão de missão clicado!")
-            if len(mission_manager.missions) < 3:
-                mission_manager.get_new_quest()
+            mission_manager.get_new_quest()
             print(f"Missões ativas: {[m.name for m in mission_manager.missions]}")
-            clicked = True  # Marcamos que o clique já foi usado
-            click_released = False
 
-        if any(m.completed for m in mission_manager.missions):
-            if quest_complete_button.draw():
-                mission_manager.complete_quest(hero)
-                clicked = True
-                click_released = False
+        if (
+            any(m.completed for m in mission_manager.missions)
+            and quest_complete_button.draw()
+        ):
+            mission_manager.complete_quest(hero)
 
-        if merchant.rect.collidepoint(pos):
-            handle_cursor(merchant, potion_plus_img, cursor_hidden, clicked, store)
-        elif blacksmith.rect.collidepoint(pos):
-            handle_cursor(blacksmith, forge_img, cursor_hidden, clicked, forge)
-        else:
+        cursor_hidden = False
+
+        npc_actions = {
+            merchant: (potion_plus_img, store),
+            blacksmith: (forge_img, forge),
+        }
+
+        for npc, (img, action) in npc_actions.items():
+            if npc.rect.collidepoint(pos):
+                handle_cursor(npc, img, cursor_hidden, False, action)
+                cursor_hidden = True
+
+                if mouse_buttons[0]:
+                    while pygame.mouse.get_pressed()[0]:
+                        pygame.event.pump()
+                    action()
+                    break
+
+        if not cursor_hidden:
             pygame.mouse.set_visible(True)
-            cursor_hidden = False
 
         clicked = handle_events()
 
@@ -171,22 +159,24 @@ def city():
 
 
 def combat(place):
+    # Globals to control battle state
     global game_over, current_fighter, action_cooldown
+
     pygame.mouse.set_visible(True)
 
-    if place == "forest":
-        list = enemy_options1
-    elif place == "cave":
-        list = enemy_options2
-
-    enemies, health_bars = selectEnemies(list)
+    # Initial Setup
+    enemy_list = enemy_options1 if place == "forest" else enemy_options2
+    enemies, health_bars = selectEnemies(enemy_list)
 
     hero_turn = 0
     enemy_turn = 0
     game_over = 0
 
-    while True:
+    def wait_for_mouse_release():
+        while pygame.mouse.get_pressed()[0]:
+            pygame.event.pump()
 
+    while True:
         basic(place, hero)
         draw_panel(hero, enemies)
 
@@ -196,12 +186,14 @@ def combat(place):
         target = None
 
         pos = pygame.mouse.get_pos()
+
+        # Run is only available on hero turn
         if game_over == 0:
-            if run_button.draw() and current_fighter == 1 and game_over == 0:
-                while pygame.mouse.get_pressed()[0]:
-                    pygame.event.pump()
+            if run_button.draw() and current_fighter == 1:
+                wait_for_mouse_release()
                 reset_battle(hero, enemies)
                 city()
+                break
 
             if current_fighter == 1:
                 draw_turn_indicator("Hero")
@@ -215,6 +207,7 @@ def combat(place):
             potion = True
 
         draw_hero_hud(hero)
+
         for i, enemy in enumerate(enemies):
             draw_update(enemy)
             health_bars[i].draw(enemy.hp)
@@ -231,7 +224,7 @@ def combat(place):
         attack, target = getTarget(enemies, pos)
 
         if game_over == 0:
-            # Player action
+            # Player
             hero_turn, game_over, current_fighter, action_cooldown = playerTurn(
                 hero,
                 hero_turn,
@@ -243,7 +236,7 @@ def combat(place):
                 potion,
             )
 
-            # Enemy action
+            # Enemy
             for count, enemy in enumerate(enemies):
                 enemy_turn, game_over, current_fighter, action_cooldown = enemyTurn(
                     enemy,
@@ -253,9 +246,9 @@ def combat(place):
                     current_fighter,
                     action_cooldown,
                     count,
-                    # missions,
                 )
                 mission_manager.check_quests(enemy)
+
             # If all fighters have had a turn then reset
             if current_fighter > total_fighters:
                 current_fighter = 1
@@ -266,13 +259,14 @@ def combat(place):
         )
 
         if game_over:
+            # Reset the counted for mission
             for enemy in enemies:
-                enemy.counted_for_mission = (
-                    False  # Permite que novos combates contem para as missões
-                )
+                enemy.counted_for_mission = False
 
+        # Eventos gerais
         clicked = handle_events()
 
+        # Atualiza tela
         pygame.display.update()
 
 
@@ -294,7 +288,6 @@ def store():
             return
 
         if potion_button.draw():
-
             potion = True
 
         if potion_plus_button.draw():
@@ -321,6 +314,14 @@ def store():
 
 def forge():
     pygame.mouse.set_visible(True)
+
+    if random.randint(1, 2) == 1:
+        msg = "Improve your armor for 15 gold"
+    else:
+        msg = "Sharpen your weapon for 10 gold"
+    message(blacksmith_city, msg, white, misc_text_group)
+
+    clicked = False
 
     while True:
 
